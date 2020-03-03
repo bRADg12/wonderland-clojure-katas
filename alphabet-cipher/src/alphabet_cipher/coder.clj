78ee6781ace26893afd5d135596bbd871bf314f5
BradleyGuest <bguest@leaporbit.com>
2020-03-03 16:07:43 -0500

    finding index

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,20 @@
 (ns alphabet-cipher.coder)
+(require '[clojure.string :as string])
+
+(defn char-range [start end]
+  (string/join (map char (range (int start) (inc (int end))))))
+
+(defn find-new-ind [factor length index]
+  (mod (- index (+ factor length)) length))
+
+(defn rotate [factor alphas]
+  (take (count alphas) (iterate (partial find-new-ind factor (count alphas)) 0)))
 
 (defn encode [keyword message]
-  "encodeme")
+  (rotate 5 (char-range \a \z)))
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-
